36cd942905fbc76c7a84426c56dbe034c41eb9a3
Arun Marria <44539041+arunmarria@users.noreply.github.com>
2019-11-26 12:14:48 -0800

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import plotly.graph_objs as go
 from dash.dependencies import Input, Output
 import dash_table
 import plotly.express as px
-import dash_bootstrap_components as dbc
+#import dash_bootstrap_components as dbc
 import altair as alt
 
 
